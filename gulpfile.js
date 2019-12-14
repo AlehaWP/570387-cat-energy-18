85920c4c402fcb4fb2924306c63b4ffbbdc06fd6
AlehaWP <nerchenko@gmail.com>
2019-12-14 15:37:50 +0300

    настройка авто сжатия

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var svgSprite = require("gulp-svg-sprites");
+var webp = require('gulp-webp');
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -35,7 +36,15 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task('webp', () =>
+    gulp.src('source/img/*.{jpg,png}')
+    .pipe(webp({
+        quality: 80,
+        preset: 'photo',
+        method: 6
+    }))
+    .pipe(gulp.dest('source/img/webp/'))
+);
 
 gulp.task('svg', function () {
   return gulp.src('source/img/*.svg')
@@ -43,8 +52,12 @@ gulp.task('svg', function () {
     mode: "symbols",
     preview: true,
     selector: "%f",
-    svgPath: "%f"
+    svg: {
+      symbols: 'sprite.svg'
+    }
   }
   ))
-  .pipe(gulp.dest("source/img"));
+  .pipe(gulp.dest("source/img/"));
 });
+
+gulp.task("start", gulp.series("css", "webp", "svg", "server"));
